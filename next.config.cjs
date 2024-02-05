c5800cc633eeee0ac77fac9d50ac32af2e3c0b9f
Clay Curry <me@claycurry.com>
2024-02-05 04:25:08 -0600

    bump

---
+++ next.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: 1000,
+    }
+  },
+
+
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/github',
+        destination: 'https://github.com/clay-curry/',
+        permanent: false
+      },
+      // Wildcard path matching
+      {
+        source: '/twitter',
+        destination: 'https://x.com/claycurry_',
+        permanent: false
+      },
+    ]
+  },
+}
